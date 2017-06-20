fc0bd2fc894c8c565db346c6a8d93504d51e3559
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2017-06-20 16:46:22 -0700

    set up validation

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let main = function(){
-    let zipCode = process.argv[2];
-    console.log(zipCode);
+function validateArgs() {
+    const validZipRegex = /^\d{5}(?:-\d{4})?$/;
+    //a valid zipcode looks like "82801" or "82801-2202"
+    //(ie, no need to worry about the case of "82801 2202")
+    if (!process.argv[2]) {
+        console.log("Error:  Please provide a zipcode");
+        return false;
+    } else if (!validZipRegex.test(process.argv[2])) {
+        console.log("Error:  Please provide a valid zipcode");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+let main = function () {
+    if (validateArgs()) {
+        let zipCode = process.argv[2];
+        console.log(zipCode);
+    } else {
+        //if we had trouble parsing argv, let's just bail
+        process.exit(1);
+    }
 
 };
 
