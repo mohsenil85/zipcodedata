32c3087f5a1549bfe78f43a3182f7ad30b39d00a
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2017-06-22 13:02:15 -0700

    finish impl

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+"use strict";
+
+const request = require("request");
+const sprintf = require("sprintf-js").sprintf;
+
+const openWeatherApiKey = "94ea6a76e3743679b7b79642d332af22";
+const openWeatherApiUrl = "http://api.openweathermap.org/data/2.5/weather";
+
+const googleMapsApiKey = "AIzaSyCXPMOnpBe_YPH2L1PcFvHVXK6Z7q0xkz4";
+const timezoneUrl = "https://maps.googleapis.com/maps/api/timezone/json";
+const elevationUrl = "https://maps.googleapis.com/maps/api/elevation/json";
+
+function fetchWeatherData(zipCode) {
+    const openWeatherOptions = {
+        uri: openWeatherApiUrl,
+        qs: {
+            APPID: openWeatherApiKey,
+            units: "imperial",
+            zip: zipCode + ",us"
+        },
+        json: true
+    };
+    return new Promise((resolve, reject) => {
+        request(openWeatherOptions, (error, response, body) => {
+            if (error) return reject(error);
+            console.log(body);
+            resolve([body.name, body.main.temp, body.coord.lat, body.coord.lon]);
+        })
+    });
+}
+
+function fetchTimezoneData(lat, lon) {
+    const timezoneOptions = {
+        uri: timezoneUrl,
+        qs: {
+            key: googleMapsApiKey,
+            timestamp: Date.now(),
+            location: '' + lat + ',' + lon
+        },
+    };
+    return new Promise((resolve, reject) => {
+        request(timezoneOptions, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body.timeZoneId);
+        })
+    });
+}
+
+function fetchElevationData(lat, lon) {
+    const elevationOptions = {
+        uri: elevationUrl,
+        qs: {
+            key: googleMapsApiKey,
+            timestamp: Date.now(),
+            location: '' + lat + ',' + lon
+        },
+    };
+    return new Promise((resolve, reject) => {
+        request(elevationOptions, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body.results[0].elevation);
+        })
+    });
+}
+
 function validateArgs() {
     const validZipRegex = /^\d{5}(?:-\d{4})?$/;
     //a valid zipcode looks like "82801" or "82801-2202"
@@ -13,13 +78,22 @@ function validateArgs() {
     }
 }
 
+function reportData(name, temp, timezome, elevation) {
+    const reportFormat = "At the location %s, the temperature is %s, the timezone is %s, and the elevation is %s.";
+    return sprintf(reportFormat, name, temp, timezome, elevation);
+}
 
-let main = function () {
+const main = async function () {
     if (validateArgs()) {
         let zipCode = process.argv[2];
-        console.log(zipCode);
+        let [name, temp, lat, lon] = await fetchWeatherData(zipCode);
+        let timezone = await fetchTimezoneData(lat, lon);
+        let elevation = await fetchElevationData(lat, lon);
+        const result = reportData(name, temp, timezone, elevation);
+        console.log(result);
+        process.exit(0);//be good unix citizens and explicitly report our success
     } else {
-        //if we had trouble parsing argv, let's just bail
+        //if we had trouble parsing argv, let's bail
         process.exit(1);
     }
 
